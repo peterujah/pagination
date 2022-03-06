8d393f742ef2e1a316d5512279cab2ea3a8bf2a3
Chigozie peter <ujahchigozie@gmail.com>
2022-03-06 16:47:08 +0800

    Update Pagination.php

--- src/Pagination.php
+++ src/Pagination.php
@@ -69,36 +69,62 @@ class Pagination {
    	 protected $urlQueries = array();
 
     /**
-     * Holds addition url query separator
-     * @var string
-     */
-     protected $and = "&amp;";
-
-    /**
      * Holds additional pagination button class name
      * @var string
      */
    	 private $addClass = "";
 
     /**
+     * Holds additional pagination ul parent class name
+     * @var string
+     */
+   	 private $parentClass = "";
+
+    /**
      * Holds boolean value to allow include of inline css style in page
      * @var bool
      */
-    	private $allowCss = false;
+    	private $allowCss = true;
 	
     /**
      * Holds inline css style for buttons display
      * @var string
      */
-    protected $css = "<style>
-        ul.pagination{ display: -ms-flexbox; display: flex; padding-left: 0; list-style: none; border-radius: .25rem; margin-top: 0; margin-bottom: 1rem;} 
-        .page-link{ padding: .5rem .75rem; margin-left: -1px; line-height: 1.25; color: #007bff; background-color: #fff; border: 1px solid #dee2e6;} 
-        .page-link:first-child { border-top-left-radius: .3rem; border-bottom-left-radius: .3rem;} 
-        .page-link:last-child { border-top-right-radius: .3rem; border-bottom-right-radius: .3rem;} 
-        .page-link.active, .page-item.active .page-link{ z-index: 3; color: #fff; background-color: #007bff; border-color: #007bff;}
-        li .page-link{ position: relative; display: block;} 
-        a.page-link{ display: inline-block;} 
-    </style>";
+   	 protected $css = "<style>
+	    ul.pagination {
+		display: -ms-flexbox;
+		display: flex;
+		padding-left: 0;
+		list-style: none;
+		border-radius: .25rem;
+		margin-top: 0;
+		margin-bottom: 1rem;
+	    }
+	    .page-link {
+		padding: .5rem .75rem;
+		margin-left: -1px;
+		line-height: 1.25;
+		color: #007bff;
+		background-color: #fff;
+		border: 1px solid #dee2e6;
+		border-top-right-radius: .3rem;
+		border-bottom-right-radius: .3rem;
+	    }
+	    li .page-link {
+		position: relative;
+		display: block;
+	    }
+	    a.page-link {
+		display: inline-block;
+	    }
+	    .page-link.active,
+	    .page-item.active .page-link {
+		z-index: 3;
+		color: #fff;
+		background-color: #007bff;
+		border-color: #007bff;
+	    }
+	</style>";
 
 	/**
      * Constructor.
@@ -160,7 +186,7 @@ class Pagination {
      */
     protected function buildQuery($link){
         if(!empty($this->urlQueries) && $link != "#"){
-            return $this->and . http_build_query(array_filter($this->urlQueries), '', $this->and);
+            return http_build_query(array_filter($this->urlQueries), '', '&amp;');
         }
         return "";
     }
@@ -170,7 +196,7 @@ class Pagination {
      * @param bool $allow boolean value to indicate if inline css will be created
      * @return $this
      */
-    public function setAllowCss($allow = false){
+    public function setAllowCss($allow = true){
         $this->allowCss = $allow;
         return $this;
     }
@@ -182,6 +208,16 @@ class Pagination {
      */
     public function setClass($class = ""){
         $this->addClass = $class;
+        return $this;
+    }
+
+    /**
+     * Sets additional links button class name
+     * @param string $class class name separate with space for multiple class names
+     * @return $this
+     */
+    public function setParentClass($class = ""){
+        $this->parentClass = $class;
         return $this;
     }
 
@@ -233,41 +269,31 @@ class Pagination {
      */
     protected function paging(){
         $build = "";
-		if($this->totalRecord > 0){
-			$this->totalPages = ceil($this->totalRecord / $this->pageLimit);
-            if($this->currentPage > 1){ 
-                $build .= $this->buttons("?n=1", "««");
-                $build .= $this->buttons("?n=".($this->currentPage - 1), "«");
-            }
-            
-            if ($this->currentPage > $this->pageTruncate + 1){
-                $build .= $this->buttons("#", "...", "disabled");
-            }
-            
-            $lines = 0;
-            for ($i = ($this->currentPage - $this->pageTruncate); $i <= ($this->currentPage + $this->pageTruncate); $i++){
-                if ($i >= 1 && $i <= $this->totalPages){
-                    if($i == $this->currentPage){
-                        $build .= $this->buttons("#", $i, "active");
-                    }else{
-                        if($lines <= $this->pageTruncate + 1){
-                            $build .= $this->buttons("?n=" . $i, $i);
-                            $lines++;
-                        }
-                    }
-                }
-            }
-
-			if($this->currentPage != $this->totalPages){
-                if ($this->currentPage < ($this->totalPages - 3)){
-                    $build .= $this->buttons("#", "...", "disabled");
-                }
-                $build .= $this->buttons("?n=" . ($this->currentPage + 1), "»");
-                $build .= $this->buttons("?n=" . $this->totalPages, "»»");
+	if($this->totalRecord > 0){
+		$this->totalPages = ceil($this->totalRecord / $this->pageLimit);
+			if($this->currentPage > 1){ 
+				$build .= $this->buttons("?n=1", "««");
+				$build .= $this->buttons("?n=".($this->currentPage - 1), "«");
 			}
+			if ($this->currentPage > $this->pageTruncate + 1){
+				$build .= $this->buttons("#", "...", "disabled");
+			}
+			for ($i = $this->currentPage - $this->pageTruncate; $i <= $this->currentPage + $this->pageTruncate; $i++){
+				if ($i >= 1 && $i <= $this->totalPages){
+					if($i == $this->currentPage){
+						$build .= $this->buttons("#", $i, "active");
+					}else{
+						$build .= $this->buttons("?n=" . $i, $i);
+					}
+				}
+			}
+		if($this->currentPage != $this->totalPages){
+			$build .= $this->buttons("?n=" . ($this->currentPage + 1), "»");
+			$build .= $this->buttons("?n=" . $this->totalPages, "»»");
 		}
-        return $build;
 	}
+	return $build;
+     }
 
     /**
      * Return pagination menu links and buttons
@@ -275,12 +301,12 @@ class Pagination {
      */
     public function get(){
         if(self::LIST == $this->buildType){
-            return '<ul class="pagination">' . $this->paging() . '</ul>';
+            return '<ul class="pagination ' . $this->parentClass . '">' . $this->paging() . '</ul>';
         }else if(self::LINK == $this->buildType){
             return $this->paging();
         }
         return false;
-	}
+    }
 
     /**
      * Preview pagination menu links and buttons in page
@@ -291,5 +317,5 @@ class Pagination {
             echo $this->css;
         }
         echo $this->get();
-	}
+    }
 }
